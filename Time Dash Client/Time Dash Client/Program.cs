8ae1503f7aace24db9e32c202c48ae6450c3291e
DuckMonster <emil.strm@gmail.com>
2014-11-29 12:46:46 +0100

    Main Framework

--- Time Dash Client/Time Dash Client/Program.cs
+++ Time Dash Client/Time Dash Client/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using Tao.FreeGlut;
 using OpenGL;
 
 class Program
 {
+	static Game game;
+
 	static void Main(string[] args)
 	{
 		Glut.glutInit();
@@ -15,6 +18,11 @@ class Program
 		Glut.glutDisplayFunc(OnDisplay);
 		Glut.glutCloseFunc(OnClose);
 
+		Glut.glutKeyboardFunc(KeyboardDown);
+		Glut.glutKeyboardUpFunc(KeyboardUp);
+
+		game = new Game();
+
 		Glut.glutMainLoop();
 	}
 
@@ -24,6 +32,17 @@ class Program
 
 	static void OnClose()
 	{
+		Garbage.Dispose();
+	}
+
+	static void KeyboardDown(byte key, int x, int y)
+	{
+		Keyboard.SetKey(key, true);
+	}
+
+	static void KeyboardUp(byte key, int x, int y)
+	{
+		Keyboard.SetKey(key, false);
 	}
 
 	static void OnRenderFrame()
@@ -31,6 +50,37 @@ class Program
 		Gl.ClearColor(1, 0, 0, 1);
 		Gl.Clear(ClearBufferMask.ColorBufferBit);
 
+		Keyboard.Update();
+
+		game.Logic();
+		game.Draw();
+
 		Glut.glutSwapBuffers();
+	}
+
+	public static ShaderProgram CreateShaderProgram(string vertexPath, string fragmentPath)
+	{
+		ShaderProgram program = new ShaderProgram(ReadFile(vertexPath), ReadFile(fragmentPath));
+		if (program.ProgramLog != "")
+		{
+			Console.WriteLine(program.ProgramLog);
+			Console.ReadKey();
+
+			throw new NullReferenceException();
+		}
+
+		program.DisposeChildren = true;
+		Garbage.Add(program);
+
+		return program;
+	}
+
+	public static string ReadFile(string fileName)
+	{
+		using (StreamReader stream = new StreamReader(fileName))
+		{
+			string line = stream.ReadToEnd();
+			return line;
+		}
 	}
 }
--- Time Dash Client/Time Dash Client/shaders/fragmentShader.cpp
+++ Time Dash Client/Time Dash Client/shaders/fragmentShader.cpp
@@ -5,15 +5,16 @@ uniform bool textureEnabled;
 uniform vec4 u_color;
 
 in vec2 uv;
-
 out vec4 fragment;
 
 void main(void)
 {
 	if (textureEnabled) {
-		fragment = u_color * texture2D(texture, uv);
+		fragment = texture2D(texture, uv) * u_color;
 	}
 	else {
 		fragment = u_color;
 	}
+
+	fragment = vec4(1, 1, 1, 1);
 }
