3a3f01ed21aad1b52fd9a7c418d8b89591e51321
DuckMonster <emil.strm@gmail.com>
2014-11-29 12:47:16 +0100

    Game Framework

---
+++ Time Dash Client/Time Dash Client/Keyboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Keyboard
+{
+	private static bool[] currentKeyData = new bool[256];
+	private static Keyboard current, previous;
+
+	public static Keyboard Current
+	{
+		get { return current; }
+	}
+	public static Keyboard Previous
+	{
+		get { return previous; }
+	}
+
+	public static void SetKey(byte k, bool s)
+	{
+		currentKeyData[k] = s;
+	}
+
+	public static void Update()
+	{
+		previous = current;
+		current = new Keyboard(currentKeyData);
+	}
+
+	private bool[] key;
+
+	public Keyboard(bool[] data)
+	{
+		key = new bool[data.Length];
+		for (int i = 0; i < key.Length; i++) key[i] = data[i];
+	}
+
+	public bool this[byte i]
+	{
+		get
+		{
+			return key[i];
+		}
+	}
+	public bool this[char i]
+	{
+		get
+		{
+			return key[i];
+		}
+	}
+}
---
+++ Time Dash Client/Time Dash Client/Log.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Log
+{
+	public static float debugTimer = 0f, debugInterval = 0.05f;
+	public static bool CanDebug
+	{
+		get
+		{
+			return debugTimer <= 0f;
+		}
+	}
+
+	public static void Debug(string text, params object[] args)
+	{
+		if (CanDebug)
+		{
+			Console.WriteLine(text, args);
+			Console.WriteLine("--");
+		}
+	}
+
+	private static int[] fpsAvg = new int[20];
+	private static int fpsAvgIndex = 0;
+
+	public static void Logic()
+	{
+		if (CanDebug) debugTimer = debugInterval;
+		debugTimer -= Game.delta;
+
+		ShowFPS();
+	}
+
+	static void ShowFPS()
+	{
+		if (CanDebug)
+		{
+			Console.Clear();
+
+			fpsAvg[fpsAvgIndex] = (int)(1.0 / Game.delta);
+
+			//Calculate average
+			int avg = 0;
+
+			for (int i = 0; i < fpsAvg.Length; i++)
+				avg += fpsAvg[i];
+
+			avg /= fpsAvg.Length;
+			//
+
+			Debug("FPS: {0:0} ~ {1}", fpsAvg[fpsAvgIndex], avg);
+
+			fpsAvgIndex = (fpsAvgIndex + 1) % fpsAvg.Length;
+		}
+	}
+}
