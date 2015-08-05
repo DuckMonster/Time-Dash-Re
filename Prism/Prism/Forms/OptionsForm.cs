cc927d7a0998139147e5b6dff52cc06cf188c120
Emil Ström <emil.strm@gmail.com>
2015-08-05 17:30:59 +0200

    Working Prism version 0.2.0

--- Prism/Prism/Forms/OptionsForm.cs
+++ Prism/Prism/Forms/OptionsForm.cs
@@ -9,99 +9,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public class EditorOptions
-{
-	bool showGrid, focusLayer, meshBorders;
-	float gridOpacity, layerOpacity, meshBorderOpacity;
-	float gridSize;
-
-	public bool ShowGrid { get { return showGrid; } set { showGrid = value; } }
-	public bool FocusLayer { get { return focusLayer; } set { focusLayer = value; } }
-	public bool MeshBorders { get { return meshBorders; } set { meshBorders = value; } }
-
-	public float GridOpacity { get { return gridOpacity; } set { gridOpacity = value; } }
-	public float GridSize { get { return gridSize; } set { gridSize = value; } }
-	public float LayerOpacity { get { return layerOpacity; } set { layerOpacity = value; } }
-	public float MeshBorderOpacity { get { return meshBorderOpacity; } set { meshBorderOpacity = value; } }
-
-	public EditorOptions()
-	{
-		Load();
-	}
-
-	public EditorOptions(bool showGrid, float gridSize, bool focusLayer, bool meshBorders,
-		float gridOpacity, float layerOpacity, float meshBorderOpacity)
-	{
-		this.showGrid = showGrid;
-		this.gridSize = gridSize;
-		this.focusLayer = focusLayer;
-		this.meshBorders = meshBorders;
-
-		this.gridOpacity = gridOpacity;
-		this.layerOpacity = layerOpacity;
-		this.meshBorderOpacity = meshBorderOpacity;
-	}
-
-	void SetDefault()
-	{
-		showGrid = true;
-		gridSize = 1f;
-		gridOpacity = 1f;
-
-		focusLayer = true;
-		layerOpacity = 0.4f;
-
-		meshBorders = true;
-		meshBorderOpacity = 1f;
-	}
-
-	public void Load()
-	{
-		try
-		{
-			IniFile file = new IniFile("config.cfg");
-
-			IniFile.Section s = file["editor"];
-			ShowGrid = s["showGrid"] == "1" ? true : false;
-			GridOpacity = Single.Parse(s["gridOpacity"], System.Globalization.NumberStyles.Any);
-			GridSize = Single.Parse(s["gridSize"], System.Globalization.NumberStyles.Any);
-
-			FocusLayer = s["focusLayer"] == "1" ? true : false;
-			LayerOpacity = Single.Parse(s["layerOpacity"], System.Globalization.NumberStyles.Any);
-
-			MeshBorders = s["meshBorders"] == "1" ? true : false;
-			MeshBorderOpacity = Single.Parse(s["meshBorderOpacity"], System.Globalization.NumberStyles.Any);
-		} catch(Exception e)
-		{
-			SetDefault();
-		}
-	}
-
-	public void Save()
-	{
-		IniFile file = new IniFile();
-
-		IniFile.Section s = file["editor"];
-
-		s["showGrid"] = showGrid ? "1" : "0";
-		s["gridOpacity"] = gridOpacity.ToString();
-		s["gridSize"] = gridSize.ToString();
-
-		s["focusLayer"] = focusLayer ? "1" : "0";
-		s["layerOpacity"] = LayerOpacity.ToString();
-
-		s["meshBorders"] = meshBorders ? "1" : "0";
-		s["meshBorderOpacity"] = meshBorderOpacity.ToString();
-
-		file.SaveTo("config.cfg");
-	}
-
-	void CreateStandard()
-	{
-		IniFile file = new IniFile("config.cfg");
-	}
-}
-
 public partial class OptionsForm : Form
 {
 	public static EditorOptions options = new EditorOptions();
@@ -169,5 +76,100 @@ public partial class OptionsForm : Form
 	protected override void OnClosing(CancelEventArgs e)
 	{
 		options.Save();
+	}
+}
+
+
+public class EditorOptions
+{
+	bool showGrid, focusLayer, meshBorders;
+	float gridOpacity, layerOpacity, meshBorderOpacity;
+	float gridSize;
+
+	public bool ShowGrid { get { return showGrid; } set { showGrid = value; } }
+	public bool FocusLayer { get { return focusLayer; } set { focusLayer = value; } }
+	public bool MeshBorders { get { return meshBorders; } set { meshBorders = value; } }
+
+	public float GridOpacity { get { return gridOpacity; } set { gridOpacity = value; } }
+	public float GridSize { get { return gridSize; } set { gridSize = value; } }
+	public float LayerOpacity { get { return layerOpacity; } set { layerOpacity = value; } }
+	public float MeshBorderOpacity { get { return meshBorderOpacity; } set { meshBorderOpacity = value; } }
+
+	public EditorOptions()
+	{
+		Load();
+	}
+
+	public EditorOptions(bool showGrid, float gridSize, bool focusLayer, bool meshBorders,
+		float gridOpacity, float layerOpacity, float meshBorderOpacity)
+	{
+		this.showGrid = showGrid;
+		this.gridSize = gridSize;
+		this.focusLayer = focusLayer;
+		this.meshBorders = meshBorders;
+
+		this.gridOpacity = gridOpacity;
+		this.layerOpacity = layerOpacity;
+		this.meshBorderOpacity = meshBorderOpacity;
+	}
+
+	void SetDefault()
+	{
+		showGrid = true;
+		gridSize = 1f;
+		gridOpacity = 1f;
+
+		focusLayer = true;
+		layerOpacity = 0.4f;
+
+		meshBorders = true;
+		meshBorderOpacity = 1f;
+	}
+
+	public void Load()
+	{
+		try
+		{
+			IniFile file = new IniFile("config.cfg");
+
+			IniFile.Section s = file["editor"];
+			ShowGrid = s["showGrid"] == "1" ? true : false;
+			GridOpacity = Single.Parse(s["gridOpacity"], System.Globalization.NumberStyles.Any);
+			GridSize = Single.Parse(s["gridSize"], System.Globalization.NumberStyles.Any);
+
+			FocusLayer = s["focusLayer"] == "1" ? true : false;
+			LayerOpacity = Single.Parse(s["layerOpacity"], System.Globalization.NumberStyles.Any);
+
+			MeshBorders = s["meshBorders"] == "1" ? true : false;
+			MeshBorderOpacity = Single.Parse(s["meshBorderOpacity"], System.Globalization.NumberStyles.Any);
+		}
+		catch (Exception e)
+		{
+			SetDefault();
+		}
+	}
+
+	public void Save()
+	{
+		IniFile file = new IniFile();
+
+		IniFile.Section s = file["editor"];
+
+		s["showGrid"] = showGrid ? "1" : "0";
+		s["gridOpacity"] = gridOpacity.ToString();
+		s["gridSize"] = gridSize.ToString();
+
+		s["focusLayer"] = focusLayer ? "1" : "0";
+		s["layerOpacity"] = LayerOpacity.ToString();
+
+		s["meshBorders"] = meshBorders ? "1" : "0";
+		s["meshBorderOpacity"] = meshBorderOpacity.ToString();
+
+		file.SaveTo("config.cfg");
+	}
+
+	void CreateStandard()
+	{
+		IniFile file = new IniFile("config.cfg");
 	}
 }
